4dd340c3f1f15c4e49de29158b1c6a73fd3ee777
Eknoor Singh <eknoor.singh@nagarro.com>
2024-10-13 12:17:57 +0530

    first commit

---
+++ src/test/java/com/redbus/testcases/CalculatorTest.java
@@ -0,0 +1,43 @@
+package com.redbus.testcases;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotSame;
+
+import org.testng.annotations.Test;
+
+import com.redbus.base.BaseClass;
+
+public class CalculatorTest extends BaseClass{
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+	@Test
+	public void testAdd() {
+		int total = 8;
+		int sum = add(4, 4);
+		assertEquals(sum, total);
+		System.out.println("Add test passed");
+	}
+
+	@Test
+	public void testFailedAdd() {
+		int total = 9;
+		int sum = add(10, 5);
+		assertNotSame(sum, total);
+		System.out.println("Add test failed");
+	}
+
+	@Test
+	public void testSub() {
+		int total = 0;
+		int sub = sub(4, 4);
+		assertEquals(sub, total);
+		System.out.println("Sub test passed");
+	}
+}
